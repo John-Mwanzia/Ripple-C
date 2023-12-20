3451e11f855b05f0c8b5518c22beea3bf23a1112
John mwanzia <johnmwanzia277@gmail.com>
2023-12-20 18:48:46 +0300

    edit sign up handler

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,34 +1,28 @@
 import { generateToken, hashPassword } from "@/modules/auth";
 import prisma from "@/modules/db";
-import { setCookie } from "cookies-next";
 import { NextResponse } from "next/server";
 
 export const POST = async (req: Request) => {
-    const request = await req.json();
-    const { phoneNumber, firstName, password } = request;
-    const user = await prisma.user.findUnique({
-        where: {
-        phoneNumber: phoneNumber,
-        },
-    });
-    if (user) {
-        return {
-        status: 409,
-        body: { message: "User already exists" },
-        };
+  const request = await req.json();
+  const { phoneNumber, firstName, password } = request;
+  const user = await prisma.user.findUnique({
+    where: {
+      phoneNumber: phoneNumber,
+    },
+  });
+  if (user) {
+    return NextResponse.json({ message: "User already exists" }, { status: 409 });
     }
-    const hashedPassword = await hashPassword(password);
-    const newUser = await prisma.user.create({
-        data: {
-        firstName,
-        phoneNumber,
-        password: hashedPassword,
-        },
-    });
-    const token = generateToken(newUser);
+    
+  const hashedPassword = await hashPassword(password);
+  const newUser = await prisma.user.create({
+    data: {
+      firstName,
+      phoneNumber,
+      password: hashedPassword,
+    },
+  });
+  const token = generateToken(newUser);
 
-    return new Response(JSON.stringify({ token }), {
-        status: 200,
-        headers: { 'Content-Type': 'application/json' },
-      });
-    }
+  return NextResponse.json({ token }, { status: 200 });
+};
