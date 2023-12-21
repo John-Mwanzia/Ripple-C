944ef05cf78f3397ad1bfa4d6b03f5a55579c3be
John mwanzia <johnmwanzia277@gmail.com>
2023-12-22 02:52:42 +0300

    add tokenTypes file

---
+++ tokenTypes.d.ts
@@ -0,0 +1,10 @@
+interface MyDecodedToken {
+    exp: number;
+    iat: number;
+    id: string;
+    isAdmin: boolean;
+    name: string;
+    phoneNumber: string;
+    referralCode: string; 
+  
+  }
