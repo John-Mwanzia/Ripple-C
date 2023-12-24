72004405c5d96e914f3b53b77fcb245cc6d3f504
John mwanzia <johnmwanzia277@gmail.com>
2023-12-24 04:07:08 +0300

    fetch specific users balances

---
+++ app/api/user/[phoneNumber]/route.ts
@@ -0,0 +1,27 @@
+import prisma from "@/modules/db";
+import { NextResponse } from "next/server";
+
+export const GET = async (req: Request, { params }) => {
+  const { phoneNumber } = params;
+  console.log(phoneNumber);
+
+  const user = await prisma.user.findUnique({
+    where: {
+      phoneNumber: phoneNumber,
+    },
+    include: {
+      Account: true,
+      referrer: true,
+      referee: true,
+    },
+  });
+  if (!user) {
+    return NextResponse.json(
+      { message: "User does not exist" },
+      { status: 404 }
+    );
+  }
+
+
+  return NextResponse.json({ data: user }, { status: 200 });
+};
---
+++ app/components/account/Header.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import React from "react";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+export default function Header({ user }) {
+  
+  const getAvatarFallback = () => {
+    if (user && user.name) {
+      return user.name.charAt(0).toUpperCase();
+    }
+    return ""; // Or any other default character you want to display
+  };
+  return (
+    <div className="flex justify-between px-4">
+      <div className="absolute top-6 text-white text-xl font-bold">
+        {user && user.phoneNumber}
+      </div>
+      <div className="absolute top-20 text-black px-3  font-bold">
+        <span>
+            <strong>Recomended Code : </strong>
+        </span>
+        {user && user.referralCode}
+      </div>
+      <div className="absolute top-4 right-4">
+        <Avatar>
+          <AvatarImage
+            src="https://github.com/shadcn.png"
+            alt="avatar"
+            width={40}
+            height={40}
+          />
+          <AvatarFallback>{getAvatarFallback()}</AvatarFallback>
+        </Avatar>
+      </div>
+    </div>
+  );
+}
