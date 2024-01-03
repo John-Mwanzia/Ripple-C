36c0ce896296575eadc545af8d71595cfd52350f
John mwanzia <johnmwanzia277@gmail.com>
2024-01-03 19:23:45 +0300

    enable filtering, sorting and visibility in admin page for users

---
+++ app/admin/[id]/columns.tsx
@@ -0,0 +1,164 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import React from "react";
+import { ArrowUpDown, MoreHorizontal } from "lucide-react";
+import { Checkbox } from "@/components/ui/checkbox";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+export type User = {
+  id: string;
+  firstName: string;
+  phoneNumber: string;
+  Account: {
+    balance: number;
+    Transaction: {
+      id: string;
+      status: string;
+      amount: number;
+      createdAt: Date;
+    }[];
+  }[];
+  investments: {
+    product: {
+      productName: string;
+      productImage: string;
+      dailyIncome: number;
+    };
+    amount: number;
+  }[];
+};
+
+export const columns: ColumnDef<User>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() ? "indeterminate" : false)
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+  },
+  {
+    accessorKey: "name",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Name
+          <ArrowUpDown
+            className={`h-4 w-4 ${
+              column.getIsSorted() ? "text-gray-900" : "text-gray-400"
+            }`}
+          />
+        </Button>
+      );
+    },
+    cell: ({ row }) => <span>{row.original.firstName}</span>,
+  },
+
+  {
+    accessorKey: "phoneNumber",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Phone Number
+          <ArrowUpDown
+            className={`h-4 w-4 ${
+              column.getIsSorted() ? "text-gray-900" : "text-gray-400"
+            }`}
+          />
+        </Button>
+      );
+    },
+    cell: ({ row }) => <span>{row.original.phoneNumber}</span>,
+  },
+  {
+    accessorKey: "balance",
+    header: () => <span>Account Balance</span>,
+    cell: ({ row }) => (
+      <span>{row.original.Account[0].balance.toFixed(2)}</span>
+    ),
+  },
+  {
+    accessorKey: "investmentsLength",
+    header: () => <span>Investments</span>,
+    cell: ({ row }) => <span>{row.original.investments.length}</span>,
+  },
+  {
+    accessorKey: "transactionsLength",
+    header: () => <span>Transactions</span>,
+    cell: ({ row }) => (
+      <span>{row.original.Account[0].Transaction.length}</span>
+    ),
+  },
+  {
+    accessorKey: "totalInvestmentAmount",
+    header: () => <span>Total Investment Amount</span>,
+    cell: ({ row }) =>
+      row.original.investments
+        .reduce((acc, investment) => acc + investment.amount, 0)
+        .toFixed(2),
+  },
+  {
+    accessorKey: "totalDailyIncome",
+    header: () => <span>Total Daily Income</span>,
+    cell: ({ row }) =>
+      row.original.investments
+        .reduce((acc, investment) => acc + investment.product.dailyIncome, 0)
+        .toFixed(2),
+  },
+  {
+    id: "actions",
+    header: () => <span>Actions</span>,
+    cell: ({ row }) => {
+      const user = row.original;
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(user.id)}
+            >
+              Copy user ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
--- app/admin/[id]/user/[userId]/page.tsx
+++ app/admin/[id]/user/[userId]/page.tsx
@@ -1,31 +1,185 @@
-import prisma from "@/modules/db";
-import React from "react";
+// import prisma from "@/modules/db";
+// import React from "react";
 
-export default async function page({ params }) {
-  const { userId } = params;
+// export default async function page({ params }) {
+//   const { userId } = params;
 
-  const user = await prisma.user.findUnique({
-    where: {
-      id: userId,
-    },
-    include: {
-      Account: {
-        include: {
-          Transaction: {
-            orderBy: {
-              createdAt: "desc",
-            },
-          },
-        },
-      },
+//   const user = await prisma.user.findUnique({
+//     where: {
+//       id: userId,
+//     },
+//     include: {
+//       Account: {
+//         include: {
+//           Transaction: {
+//             orderBy: {
+//               createdAt: "desc",
+//             },
+//           },
+//         },
+//       },
 
-      investments: {
-        include: {
-          product: true,
-        },
-      },
-    },
-  });
+//       investments: {
+//         include: {
+//           product: true,
+//         },
+//       },
+//     },
+//   });
 
-  return <div>confirm payment for {userId}</div>;
-}
+//   return <div>confirm payment for {userId}</div>;
+// }
+
+// import UserData from "@/app/components/admin/UserData";
+// import prisma from "@/modules/db";
+// import Image from "next/image";
+// import React from "react";
+
+// export default async function page({ params }) {
+//   // this id is only for showing a welcome message  to the admin
+//   const { id } = params;
+
+//   //   fetch every collection and display them in a table
+
+//   const response = await prisma.user.findMany({
+//     include: {
+//       Account: {
+//         include: {
+//           Transaction: true,
+//         },
+//       },
+//       investments: {
+//         include: {
+//           product: true,
+//         },
+//       },
+//     },
+//   });
+
+//   return (
+//     <div className=" min-h-screen">
+//       <UserData data={response} />
+//       <div className=" mx-auto px-4 py-8">
+//         <h1 className="text-3xl font-bold text-white">Welcome Admin {id}</h1>
+//         <table className="min-w-full border-2 border-gray-500 rounded-lg overflow-hidden">
+//           <thead className="bg-gray-200 text-gray-600">
+//             <tr>
+//               <th className="px-4 py-2 text-left">Name</th>
+//               <th className="px-4 py-2">Phone Number</th>
+//               <th className="px-4 py-2">Account Balance</th>
+//               <th className="px-4 py-2">Investments</th>
+//               <th className="px-4 py-2">Total investment amount</th>
+//               <th className="px-4 py-2">Total daily income</th>
+//               <th className="px-4 py-2"> Transactions</th>
+//               <th className="px-4 py-2">Actions</th>
+//             </tr>
+//           </thead>
+//           <tbody>
+//             {response.map((user) => (
+//               <tr
+//                 key={user.id}
+//                 className="bg-white border-b border-gray-200 hover:bg-gray-100"
+//               >
+//                 <td className="px-4 py-2">{user.firstName}</td>
+//                 <td className="px-4 py-2">{user.phoneNumber}</td>
+//                 <td className="px-4 py-2">{user.Account[0].balance}</td>
+//                 <td className="px-4 py-2">
+//                   {/* Investments */}
+//                   <div className="flex flex-col">
+//                     {user.investments.map((investment, index) => (
+//                       <div
+//                         key={index}
+//                         className="flex border border-gray-300 mb-4 p-4"
+//                       >
+//                         <div className="grid grid-cols-4 gap-4">
+//                           <div className="flex flex-col">
+//                             <h3 className="font-bold mb-1">Product Name</h3>
+//                             <span className="overflow-hidden overflow-ellipsis whitespace-nowrap">
+//                               {investment.product.productName}
+//                             </span>
+//                           </div>
+//                           <div className="flex flex-col">
+//                             <h3 className="font-bold mb-1">Amount</h3>
+//                             <span>{investment.amount}</span>
+//                           </div>
+//                           <div className="flex flex-col">
+//                             <h3 className="font-bold mb-1"> Image</h3>
+//                             <img
+//                               src={investment.product.productImage}
+//                               width={30}
+//                               height={30}
+//                               alt="product image"
+//                             />
+//                           </div>
+//                           <div className="flex flex-col">
+//                             <h3 className="font-bold mb-1">Daily Income</h3>
+//                             <span>{investment.product.dailyIncome}</span>
+//                           </div>
+//                         </div>
+//                       </div>
+//                     ))}
+//                   </div>
+//                 </td>
+//                 <td className="px-4 py-2">
+//                   {user.investments.reduce(
+//                     (acc, investment) => acc + investment.amount,
+//                     0
+//                   )}
+//                 </td>
+//                 <td className="px-4 py-2">
+//                   {/* add all daily incomes */}
+//                   {user.investments.reduce(
+//                     (acc, investment) => acc + investment.product.dailyIncome,
+//                     0
+//                   )}
+//                 </td>
+//                 <td className="px-4 py-2">
+//                   <div className="flex flex-col">
+//                     {user.Account[0].Transaction.map((transaction) => (
+//                       <div
+//                         key={transaction.id}
+//                         className="flex  flex-col border border-gray-300 mb-4 p-4"
+//                       >
+//                         <div className="flex- flex-col gap-4 w-full">
+//                           <div className="flex gap-4">
+//                             <h3 className="font-bold"> ID</h3>
+//                             <span className="overflow-hidden overflow-ellipsis whitespace-nowrap">
+//                               {transaction.id}
+//                             </span>
+//                           </div>
+//                           <div className="flex gap-4  ">
+//                             <h3 className="font-bold">Status</h3>
+//                             <span className="text-[#E95514] font-semibold">
+//                               {transaction.status}
+//                             </span>
+//                           </div>
+//                           <div className="flex gap-4">
+//                             <h3 className="font-bold">Amount</h3>
+//                             <span>{transaction.amount}</span>
+//                           </div>
+//                           <div className="flex gap-4">
+//                             <h3 className="font-bold"> Date</h3>
+//                             <span className="whitespace-nowrap">
+//                               {new Date(transaction.createdAt).toLocaleString()}
+//                             </span>
+//                           </div>
+//                         </div>
+//                       </div>
+//                     ))}
+//                   </div>
+//                 </td>
+//                 <td className="px-4 py-2">
+//                   {/* Add actions buttons or links */}
+//                   {/* Example: */}
+//                   <button className="bg-blue-500 text-white px-3 py-1 rounded">
+//                     View Details
+//                   </button>
+//                 </td>
+//               </tr>
+//             ))}
+//           </tbody>
+//         </table>
+//       </div>
+//     </div>
+//   );
+// }
