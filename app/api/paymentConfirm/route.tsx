d9d0f57c9754d64d019e736a42b49863923db435
John mwanzia <johnmwanzia277@gmail.com>
2024-01-05 18:32:05 +0300

    handle inviterevenue awarding

--- app/api/paymentConfirm/route.tsx
+++ app/api/paymentConfirm/route.tsx
@@ -1,6 +1,40 @@
 import prisma from "@/modules/db";
 import { NextResponse } from "next/server";
 
+async function updateTransactionStatus(id: string, status) {
+  try {
+    const updatedTransaction = await prisma.transaction.update({
+      where: {
+        id,
+      },
+      data: {
+        status,
+      },
+    });
+    return updatedTransaction;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+}
+
+async function updateAccountBalance(id: string, amount) {
+  try {
+    const updatedAccount = await prisma.account.update({
+      where: {
+        id,
+      },
+      data: {
+        balance: amount,
+      },
+    });
+    return updatedAccount;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+}
+
 export const POST = async (req: Request, res: Response) => {
   const { userId, amount, id } = await req.json();
 
@@ -52,27 +86,134 @@ export const POST = async (req: Request, res: Response) => {
     });
   }
 
-  const updatedTransaction = await prisma.transaction.update({
-    where: {
-      id,
-    },
-    data: {
-      status: "COMPLETED",
-    },
-  });
+  // update transaction status
+  const updatedTransaction = await updateTransactionStatus(id, "COMPLETED");
 
   //  Update user account balance
 
   const acccount = user.Account[0];
 
-  const updatedAccount = await prisma.account.update({
-    where: {
-      id: acccount.id,
-    },
-    data: {
-      balance: acccount.balance + amount,
-    },
-  });
+  const updatedAccount = await updateAccountBalance(
+    acccount.id,
+    acccount.balance + amount
+  );
 
+  try {
+    // this invite revenue will be awarded to the referrer(10%) and the secondary referrer(4%) to the current user
+    // but this revenue is only awarded when the current user successfully his payment is completed and
+    // on the only the first completed transaction, those other consecutive payments no one will be awarded invite revenue
+
+    // check if there is any other first transaction that has been completed by the user or assign this transaction as the first completed transaction
+    const firstCompletedTransaction = await prisma.transaction.findFirst({
+      where: {
+        accountId: acccount.id,
+        status: "COMPLETED",
+      },
+      orderBy: {
+        createdAt: "asc",
+      },
+    });
+
+    // if there is no other completed transaction then this transaction will be the first completed transaction
+    if (!firstCompletedTransaction) {
+      // check if the user has a referrer
+      if (user.referrerId) {
+        // get the referrer
+        const referrer = await prisma.user.findUnique({
+          where: {
+            id: user.referrerId,
+          },
+          include: {
+            Account: {
+              include: {
+                Transaction: {
+                  orderBy: {
+                    createdAt: "desc",
+                  },
+                },
+              },
+            },
+          },
+        });
+
+        // check if the referrer has a secondary referrer
+        if (referrer.referrerId) {
+          // get the secondary referrer
+          const secondaryReferrer = await prisma.user.findUnique({
+            where: {
+              id: referrer.referrerId,
+            },
+            include: {
+              Account: {
+                include: {
+                  Transaction: {
+                    orderBy: {
+                      createdAt: "desc",
+                    },
+                  },
+                },
+              },
+            },
+          });
+
+          // award the referrer 10% of the amount of the transaction to the account and also the invite revenue of the referrer will be updated
+          const updatedReferrerAccount = await prisma.account.update({
+            where: {
+              id: referrer.Account[0].id,
+            },
+            data: {
+              balance: {
+                increment: amount * 0.1,
+              },
+            },
+          });
+
+          const updatedReferrer = await prisma.user.update({
+            where: {
+              id: referrer.id,
+            },
+            data: {
+              inviteRevenue: {
+                increment: amount * 0.1,
+              },
+            },
+          });
+
+          // award the secondary referrer 4% of the amount of the transaction to the account
+          // and also the invite revenue of the secondary referrer will be updated
+          const updatedSecondaryReferrerAccount = await prisma.account.update({
+            where: {
+              id: secondaryReferrer.Account[0].id,
+            },
+            data: {
+              balance: {
+                increment: amount * 0.04,
+              },
+            },
+          });
+
+          const updatedSecondaryReferrer = await prisma.user.update({
+            where: {
+              id: secondaryReferrer.id,
+            },
+            data: {
+              inviteRevenue: {
+                increment: amount * 0.04,
+              },
+            },
+          });
+        }
+      }
+    }
+
+    // if there is another completed transaction, this transaction will not be the first completed transaction,
+    // and no one will be awarded invite revenue, just continue
+  } catch (error) {
+    // Handle errors here (e.g., log them, send error response, etc.)
+    console.error("Error occurred:", error);
+    // Respond with an error message if needed
+    return NextResponse.json({ status: "error", message: "An error occurred" });
+  }
+  // Continue with the rest of the code or respond with success message
   return NextResponse.json({ status: "success", message: "Payment confirmed" });
 };
