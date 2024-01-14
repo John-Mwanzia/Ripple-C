cc4cbb306c4eb3542857ce729d0d5bec46fe0ac9
John mwanzia <johnmwanzia277@gmail.com>
2024-01-14 19:44:16 +0300

    change homepage layout

--- app/components/home/Products.tsx
+++ app/components/home/Products.tsx
@@ -148,7 +148,7 @@ export default function Products({ products }) {
                 ) : (
                   <button
                     onClick={() => handleBuyNow(product, index)} // Pass index here
-                    className="bg-[#E95514]/80 text-white px-4 py-2 rounded-md"
+                    className="bg-[#4294FF]/80 text-white px-4 py-2 rounded-md"
                   >
                     Buy Now
                   </button>
