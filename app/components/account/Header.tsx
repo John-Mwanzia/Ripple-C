3e20c28ef17d96cedf1027d490b7d0eca0fee33b
John mwanzia <johnmwanzia277@gmail.com>
2024-01-18 21:29:40 +0300

    change account user avatar

--- app/components/account/Header.tsx
+++ app/components/account/Header.tsx
@@ -5,7 +5,6 @@ import React from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export default function Header({ user }) {
-  
   const getAvatarFallback = () => {
     if (user && user.name) {
       return user.name.charAt(0).toUpperCase();
@@ -19,14 +18,14 @@ export default function Header({ user }) {
       </div>
       <div className="absolute top-20 text-black px-3  font-bold">
         <span>
-            <strong>Recomended Code : </strong>
+          <strong>Recomended Code : </strong>
         </span>
         {user && user.referralCode}
       </div>
       <div className="absolute top-4 right-4">
         <Avatar>
           <AvatarImage
-            src="https://github.com/shadcn.png"
+            src="https://cdn-icons-png.flaticon.com/128/3177/3177440.png"
             alt="avatar"
             width={40}
             height={40}
